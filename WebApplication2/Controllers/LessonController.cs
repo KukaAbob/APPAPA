290f3ca6d3dec10c80374fadc29ea181bdca04c6
KukaAbob <146316346+KukaAbob@users.noreply.github.com>
2024-10-17 14:48:37 +0300

    Add files via upload

---
+++ WebApplication2/Controllers/LessonController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using YourNamespace;
+
+[ApiController]
+[Route("api/[controller]")]
+public class LessonController : ControllerBase
+{
+	private readonly DataBaseService _databaseService;
+
+	public LessonController(DataBaseService databaseService)
+	{
+		_databaseService = databaseService;
+	}
+
+	// Получение уроков по UIN учителя
+	[HttpGet("teacher/{teacherUin}")]
+	public async Task<IActionResult> GetLessonsByTeacherUin(string teacherUin)
+	{
+		var lessons = await _databaseService.GetLessonsByTeacherUinAsync(teacherUin);
+		if (lessons == null)
+		{
+			return NotFound();
+		}
+		return Ok(lessons);
+	}
+
+	// Получение урока по ID
+	[HttpGet("{lessonId}")]
+	public async Task<IActionResult> GetLessonById(String lessonId)
+	{
+		var lesson = await _databaseService.GetLessonByIdAsync(lessonId);
+		if (lesson == null)
+		{
+			return NotFound();
+		}
+		return Ok(lesson);
+	}
+
+	// Получение всех уроков
+	[HttpGet]
+	public async Task<IActionResult> GetAllLessons()
+	{
+		var lessons = await _databaseService.GetAllLessonsAsync();
+		return Ok(lessons);
+	}
+}
